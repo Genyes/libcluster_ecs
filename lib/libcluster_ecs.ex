56860055dd97ea6f88d98aa02cbd730a57fb6f6b
fm3d-work <fm3d-work@users.noreply.github.com>
2021-11-06 03:09:06 -0700

    `libcluster_ec2.ex` → `libcluster_ecs.ex`

---
+++ lib/libcluster_ecs.ex
@@ -0,0 +1,31 @@
+defmodule ClusterECS do
+  use Tesla
+
+  @moduledoc File.read!("#{__DIR__}/../README.md")
+
+  plug(Tesla.Middleware.BaseUrl, "http://169.254.169.254/latest/meta-data")
+
+  @doc """
+    Queries the local EC2 instance metadata API to determine the instance ID of the current instance.
+  """
+  @deprecated "Not yet ported"
+  @spec local_instance_id() :: binary()
+  def local_instance_id do
+    case get("/instance-id/") do
+      {:ok, %{status: 200, body: body}} -> body
+      _ -> ""
+    end
+  end
+
+  @doc """
+    Queries the local EC2 instance metadata API to determine the aws resource region of the current instance.
+  """
+  @deprecated "Not yet ported"
+  @spec instance_region() :: binary()
+  def instance_region do
+    case get("/placement/availability-zone/") do
+      {:ok, %{status: 200, body: body}} -> String.slice(body, 0..-2)
+      _ -> ""
+    end
+  end
+end
